6b1e4e17b5b3b59e6d20a103b59367b5c8d5c01b
Caleb Sawtell <caleb.sawtell@gmail.com>
2015-12-16 17:19:38 +1300

    action to hide modal

--- app/pods/components/stakeholder-modal/component.js
+++ app/pods/components/stakeholder-modal/component.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['stakeholder-modal'],
   classNameBindings: ['visible'],
-  visible: true
+  visible: true,
+  actions:{
+    close:function(){
+      this.get("setStakeholderUndefined")();
+      this.set("visible",false);
+    }
+  }
 });
