0163d2c55649ba27d0fc0a02b0972ffcb9c977c4
will-sklenars <will.sklenars@gmail.com>
2015-12-16 16:14:11 +1300

    connecting lines and target now hide when there is no selected stakeholder

--- app/pods/components/scatter-cube/connectingLine.js
+++ app/pods/components/scatter-cube/connectingLine.js
@@ -2,7 +2,7 @@ export default function ConnectingLine (opts) {
   this.a = opts.a
   this.b = opts.b
 
-  this.material = new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 2 });
+  this.material = new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 1 });
   this.mesh = this.createMesh()
 }
 
