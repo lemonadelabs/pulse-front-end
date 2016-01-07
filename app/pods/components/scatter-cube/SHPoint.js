5f6da15736efb113a804763c8f172416715f9ea1
will-sklenars <will.sklenars@gmail.com>
2016-01-07 14:53:51 +1300

    sHPoints are now being plotted on the correct axis

--- app/pods/components/scatter-cube/SHPoint.js
+++ app/pods/components/scatter-cube/SHPoint.js
@@ -34,8 +34,8 @@ SHPoint.prototype.createMesh = function(noOfWeeks) {
   // sets the position for each mesh
   var position = new THREE.Vector3();
   var x = (this.weeks[noOfWeeks].power) * 1.8  + 0.1
-  var y = (this.weeks[noOfWeeks].vital) * 1.8  + 0.1
-  var z = (this.weeks[noOfWeeks].support) * 1.8  + 0.1
+  var y = (this.weeks[noOfWeeks].support) * 1.8  + 0.1
+  var z = (this.weeks[noOfWeeks].vital) * 1.8  + 0.1
 
   point.position.set(x, y, z)
 
@@ -44,8 +44,8 @@ SHPoint.prototype.createMesh = function(noOfWeeks) {
 
 SHPoint.prototype.animate = function(week) {
   var x = (this.weeks[week].power) * 1.8  + 0.1
-  var y = (this.weeks[week].vital) * 1.8  + 0.1
-  var z = (this.weeks[week].support) * 1.8  + 0.1
+  var y = (this.weeks[week].support) * 1.8  + 0.1
+  var z = (this.weeks[week].vital) * 1.8  + 0.1
 
   var self = this
   var xTween = new TWEEN.Tween(this.mesh.position)
