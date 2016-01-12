a8f924ee32dbb8ab6db36630750659f5ce0f72c4
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-01-12 14:29:09 +1300

    hide viewmode window when there is no stakeholder selected

--- app/pods/components/stakeholder-view-mode-window/component.js
+++ app/pods/components/stakeholder-view-mode-window/component.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['stakeholder-view-mode-window','fancy-corners'],
   classNameBindings: ['visible','ignore-pointer'],
-  visible: true,
+  visible: false,
   connections:true,
   distribution:false,
   history:false,
-  "ignore-pointer":false,
+  "ignore-pointer":true,
   observeConnections:function(){
     this.get("toggleConnections")();
   }.observes("connections"),
@@ -16,5 +16,15 @@ export default Ember.Component.extend({
   }.observes("distribution"),
   observeHistory:function(){
     this.get("toggleHistory")();
-  }.observes("history")
+  }.observes("history"),
+  showWindowOnSelection:function(){
+    if(this.get("selectedStakeholder")!== undefined && this.get("visible") === false){
+      this.set("visible",true);
+      this.set("ignore-pointer",false);
+    }
+    else if(this.get("selectedStakeholder") === undefined && this.get("visible") === true){
+      this.set("visible",false);
+      this.set("ignore-pointer",true);
+    }
+  }.observes("selectedStakeholder")
 });
