12ea044364ed611d5876105072de6396459b23a0
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-01-14 17:57:55 +1300

    added masonary and finished layout for project cards

--- app/pods/components/select-project-component/component.js
+++ app/pods/components/select-project-component/component.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement: function() {
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      $('.project-container').masonry({
+        // options
+        itemSelector: '.project-card',
+        // columnWidth: 200
+        "gutter": 20,
+        isFitWidth: true
+      });
+    });
+  }
 });
