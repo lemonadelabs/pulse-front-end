84a5628470e71d2c624b5e40741fa37f3cb16ada
will-sklenars <will.sklenars@gmail.com>
2016-03-11 10:56:25 +1300

    made arrow hit boxes bigger

--- app/pods/components/scatter-cube/arrowHitbox.js
+++ app/pods/components/scatter-cube/arrowHitbox.js
@@ -10,12 +10,12 @@ export default function arrowHitbox (opts) {
   matrix.setPosition(position)
 
   if (arrowType === 'cornerArrows')  {
-    var geometry = new THREE.PlaneGeometry(1/8, 1/4, 4, 4);
+    var geometry = new THREE.PlaneGeometry(0.15, 1/4, 4, 4);
     var mesh = new THREE.Mesh(geometry, opts.material)
     mesh.applyMatrix(matrix)
     return mesh
   } else if (arrowType === 'sideArrows') {
-    var geometry = new THREE.PlaneGeometry(0.08, 0.29, 4, 4);
+    var geometry = new THREE.PlaneGeometry(0.13, 0.3, 4, 4);
     var mesh = new THREE.Mesh(geometry, opts.material)
     mesh.applyMatrix(matrix)
     return mesh
